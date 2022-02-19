d6e430a269872714a6759f53e924d243e446104a
ch450 <ch450@duke.edu>
2022-02-19 17:05:31 -0500

    writing #53-#61 for sending players' neighbors to players

--- ringmaster.c
+++ ringmaster.c
@@ -15,6 +15,10 @@ int main(int argc, char * argv[]){
     int port_num = argv[1];
     int num_players = argv[2];
     int num_hops = argv[3];
+
+    if(num_players < 1){
+        fprintf(stderr, "number of players is less than 1\n");
+    }
     // Building the server
     int status;
     struct addrinfo hints;
@@ -42,20 +46,54 @@ int main(int argc, char * argv[]){
     }
 
     freeaddrinfo(res);
-    gamesetting(sockfd, num_players, port_num);
+    int * whole_players;
+    whole_players = gamesetting(sockfd, num_players, port_num, whole_players);
 
-    Potato potato;
+    // send information to players to let them know their neighbors
+    int Length = sizeof(whole_players) / sizeof(int);
+    if(Length > 0){
+       for(int i = 0 ; i < Length; i++){
+            int right_neighbor = i == (Length - 1)? 0 : (i + 1);
+            int left_neighbor = i == 0 ? Length - 1 : (i - 1);
+            int array[2] = {right_neighbor, left_neighbor};
+            send(sockfd, &array, sizeof(array), 0); //////////////////////////////////At here
+       } 
+    }
+    
+    // start game now!
+    srand(0);
+    int random_player = rand() % num_players;
+    Potato potato; 
+    potato.hops = num_hops;
+    printf("Ready to start the game, sending potato to player %d\n", random_player);
+    send(sockfd, & potato, sizeof(potato), 0);
 }
 
-void gamesetting(int sockfd, int num_players, int port_num){
+int * gamesetting(int sockfd, int num_players, int port_num, int *whole_players){
     for(int i = 0 ; i < num_players; i++){
-        int newplayer;
         struct sockaddr_storage their_addr;
         socklen_t addr_size;
-        addr_size = sizeof their_addr;
+        addr_size = sizeof(their_addr);
         int new_fd;
         if((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) == -1){
             fprintf(stderr, "accept error: %s\n", gai_strerror(new_fd));
         }
+        int player_port;
+        int num_bytes = send(new_fd, &i, sizeof(i), 0); // Let the player know its id
+        if(num_bytes != sizeof(i)){
+            fprintf(stderr, "sned error, the number_bytes is not match: %s\n", gai_strerror(num_bytes));
+        }
+        recv(new_fd, &player_port, sizeof(player_port), 0);
+        if(player_port < 0){
+            fprintf(stderr, "player_port received from client is not correct: %s\n", gai_strerror(player_port));
+        } else if (player_port == 0){
+            fprintf(stderr, "The connection with the client has some problem:  %s\n", gai_strerror(player_port));
+        }
+        whole_players = realloc(whole_players, (i + 1) * sizeof(*whole_players));
+        whole_players[i] = player_port;
+        printf("Player %d is ready to play\n", i);
     }
+    return whole_players;
 }
+
+// the return type of the gamesetting should be modified
