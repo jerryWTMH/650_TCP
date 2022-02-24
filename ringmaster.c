177fecda3a5e3658eb446c7c53d2e04e6ed0cb64
ch450 <ch450@duke.edu>
2022-02-24 18:07:43 -0500

    leak 3

--- ringmaster.c
+++ ringmaster.c
@@ -81,6 +81,7 @@ void game_setting(int sockfd, int num_players, const char * port_num, int *whole
         whole_players_ip = (char **)realloc(whole_players_ip, (i + 1) * sizeof(*whole_players_ip));
         whole_players_fd[i] = new_fd;
         whole_players_port[i] = player_port;
+        // printf("whole_players_port[%d]: %d\n",i, whole_players_port[i]);
         whole_players_ip[i] = ip_recv;
         printf("Player %d is ready to play\n", i);
     }
@@ -88,15 +89,22 @@ void game_setting(int sockfd, int num_players, const char * port_num, int *whole
 
 void send_neighbor_info_to_players(int num_players, int * whole_players_fd, int * whole_players_port, char ** whole_players_ip){
     int i = 0;
+    // for(int i = 0; i < num_players; i++){
+    //     printf("whole_players_port[%d]: %d\n", i , whole_players_port[i]);
+    // }
     while(i < num_players){
-        int neighbor_id = (i) % num_players;
+        int neighbor_id = (i ) % num_players;
         // int neighbor_id = (i) % num_players;
+        
         int neighbor_port = whole_players_port[neighbor_id];
         char neighbor_ip[200];
         memset(neighbor_ip, 0, sizeof(neighbor_ip));
+        // printf("check initialize: %s\n", whole_players_ip[neighbor_id]);
+        // char * neighbor_ip = strdup(whole_players_ip[neighbor_id]);
         strcpy(neighbor_ip, whole_players_ip[neighbor_id]);
         send(whole_players_fd[(i+1)% num_players], &neighbor_port, sizeof(neighbor_port), 0);
         send(whole_players_fd[(i+1)%num_players], &neighbor_ip, sizeof(neighbor_ip), 0);
+        // send(whole_players_fd[(i+1)%num_players], neighbor_ip, sizeof(*neighbor_ip), 0);
         i++;
         
     }
@@ -108,6 +116,8 @@ void send_to_first_player(Potato * potato, int num_players, int * whole_players_
     
     printf("Ready to start the game, sending potato to player %d\n", random_player);
     // printf("potato.counter: %d, potato.hops: %d\n", potato->counter, potato->hops);
+    printf("%d\n", whole_players_fd[random_player]);
+    printf("sizeof potato: %ld\n", sizeof(*potato));
     send(whole_players_fd[random_player], potato, sizeof(*potato), 0);
 }
 
@@ -164,14 +174,63 @@ int main(int argc, char * argv[]){
     // Game setting
     int * whole_players_fd = (int *)malloc(sizeof(int));
     int * whole_players_port = (int *)malloc(sizeof(int));
-    char ** whole_players_ip = (char **)malloc(sizeof(char));
-    game_setting(sockfd, num_players, port_num, whole_players_fd, whole_players_port, whole_players_ip);
+    char ** whole_players_ip = (char **)malloc(sizeof(char *));
+    //
+    for(int i = 0 ; i < num_players; i++){
+        // Prepare to accept client's address from client
+        struct sockaddr_storage their_addr;
+        socklen_t addr_size;
+        addr_size = sizeof(their_addr);
+        int new_fd;
+        if((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) == -1){
+            fprintf(stderr, "accept error: %s\n", gai_strerror(new_fd));
+        }
+
+        int player_port;
+        char * ip_recv = (char * )malloc(200);
+        int num_bytes = send(new_fd, &i, sizeof(i), 0); // Let the player know its id
+        if(num_bytes != sizeof(i)){
+            fprintf(stderr, "send error, the number_bytes is not match: %s\n", gai_strerror(num_bytes));
+        }
+        // set the total number of the players
+        if((num_bytes = send(new_fd, &num_players, sizeof(num_players), 0) == -1)){
+            fprintf(stderr, "send error, the number_bytes is not match: %s\n", gai_strerror(num_bytes));
+        }
+
+        // THE FIRST RECV for PORT
+        recv(new_fd, &player_port, sizeof(player_port), 0);
+        // printf("player_port: %d", player_port);
+         if(player_port < 0){
+            fprintf(stderr, "player_port received from client is not correct: %s\n", gai_strerror(player_port));
+        } else if (player_port == 0){
+            fprintf(stderr, "The connection with the client has some problem:  %s\n", gai_strerror(player_port));
+        }
+
+        // THE SECOND RECV for IP
+        int amount; // collect the hostname(or ip address) from player
+        amount = recv(new_fd, ip_recv, 200, MSG_WAITALL);
+        if(i >= 1){
+            whole_players_fd = (int *)realloc(whole_players_fd, (i + 1) * sizeof(*whole_players_fd));
+            whole_players_port = (int *)realloc(whole_players_port, (i + 1) * sizeof(*whole_players_port));
+            whole_players_ip = (char **)realloc(whole_players_ip, (i + 1) * sizeof(*whole_players_ip));
+        }
+        whole_players_fd[i] = new_fd;
+        whole_players_port[i] = player_port;
+        // printf("whole_players_port[%d]: %d\n",i, whole_players_port[i]);
+        whole_players_ip[i] = ip_recv;
+        printf("Player %d is ready to play\n", i);
+    }
+    //
+    // game_setting(sockfd, num_players, port_num, whole_players_fd, whole_players_port, whole_players_ip);
     send_neighbor_info_to_players(num_players, whole_players_fd, whole_players_port, whole_players_ip);
     
     // start game now!
+    
     Potato potato; 
+    memset(&potato, 0, sizeof(potato));
     potato.hops = num_hops;
     potato.counter = 0;
+    
     if(num_hops > 0){
         // send the potato to the first random player!
         send_to_first_player(&potato, num_players, whole_players_fd);   
@@ -180,6 +239,7 @@ int main(int argc, char * argv[]){
         // send potato with num_hops 0 to all players to shut down
         for (int i = 0; i < num_players; i++) {
             Potato potato_zero;
+            memset(&potato_zero, 0, sizeof(potato_zero));
             potato_zero.hops = 0;
             send(whole_players_fd[i], &potato_zero, sizeof(potato_zero), 0);
         }
@@ -192,5 +252,11 @@ int main(int argc, char * argv[]){
         close(sockfd);
         return 0;
     }
+    free(whole_players_fd);
+    free(whole_players_ip);
+    for(int i = 0 ; i < num_players; i++){
+        free(whole_players_ip[i]);
+    }
+    free(whole_players_ip);
     
 }
